caa2f39c743cff0b5d4228aa14e5e26899dd8da8
wanderer <mjbecze@gmail.com>
2012-10-30 02:16:00 -0400

    added setup

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
-from distutils.core import setup
+# http://packages.python.org/distribute/setuptools.html
+# http://diveintopython3.org/packaging.html
+# http://wiki.python.org/moin/CheeseShopTutorial
+# http://pypi.python.org/pypi?:action=list_classifiers
+
+from ez_setup import use_setuptools
+use_setuptools(version='0.6c11')
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='event_map',
     version='0.1.0',
     author='M. J. Becze',
     author_email='mjbecze@riseup.net',
-    packages=['towelstuff', 'towelstuff.test'],
-    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    url='http://pypi.python.org/pypi/TowelStuff/',
-    license='LICENSE.txt',
-    description='Useful towel-related stuff.',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "Django >= 1.1.1",
-        "caldav == 0.1.4",
+    description          = "event_map",
+    long_description     = read('README.rst'),
+    license              = 'GNU AGPL v3 or later',
+    install_requires     = ['Django>=1.4', 'south>=0.7.5', 
+                            'django-celery', 'celery-with-redis','django-uuidfield'],
+    zip_safe             = False,
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Communications",
+        "Topic :: Internet :: WWW/HTTP"
     ],
 )
