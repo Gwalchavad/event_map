be3465bf8c2644942eb961dfbe57cdf36a74c90d
wanderer <mjbecze@gmail.com>
2013-02-09 12:17:28 -0500

    aded r.js build file

---
+++ event_map/static/app.build.js
@@ -0,0 +1,24 @@
+({
+    mainConfigFile: "src/js/main.js",
+    //name: "main",
+    baseUrl: "./js",
+    appDir: "./src",
+    dir: "./build",
+    optimize: "uglify2",
+    preserveLicenseComments: false,
+    //removeCombined: true,
+    //findNestedDependencies: true,
+    pragmas: {
+        debug: false
+    },
+    modules: [
+        {
+            name: "main",
+            include: ["buildUrl","../../components/require/require"]
+        },
+        {
+            name: "views/list",
+                include: ["views/list_info"]
+        }
+    ]
+})
