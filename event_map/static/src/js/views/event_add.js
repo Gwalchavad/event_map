abb6b37e9ad82e0d2a850f7021b264758d4fbca1
wanderer <mjbecze@gmail.com>
2013-02-21 12:59:00 -0500

    fixed edit event

--- event_map/static/src/js/views/event_add.js
+++ event_map/static/src/js/views/event_add.js
@@ -1,8 +1,9 @@
-/*global define alert confirm app*/
+/*global define alert confirm app L*/
 define([
     'jquery',
     'underscore',
     'backbone',
+    'moment',
     'utils',
     'geocode',
     'settings',
@@ -10,7 +11,7 @@ define([
     'hbs!../../templates/event_add',
     'timeDatePicker'
   // Load our app module and pass it to our definition function
-], function($,_,Backbone,Utils,geocode,settings,map,temp_event_add){
+], function($,_,Backbone, moment, Utils, geocoder,settings,map,temp_event_add){
     "use strict";
     var EventAddView = Backbone.View.extend({
         tagname: "div",
@@ -22,8 +23,7 @@ define([
             "click .cancel": "cancel",
             "click #delete_event": "delete_event"
         },
-        initialize: function(options) {
-            this.constructor.__super__.initialize.apply(this, [options]);
+        initialize: function() {
             this.model.on("invalid", function(model, errors) {
                 _.each(errors, function(error, key) {
                     if(key == "latlng"){
@@ -31,8 +31,10 @@ define([
                         map.marker
                             .bindPopup("<b>You Need To Move The Maker<br> To Where This Event Is Happening</b>")
                             .openPopup();
+                    }else{
+                        $("#" + key + "_error").show().html(error);
+                        $("#" + key).addClass("error");
                     }
-                    $("#" + key + "_error").show().html(error);
                 });
             });
         },
@@ -47,9 +49,9 @@ define([
                 context.location_point.lng = context.location_point.coordinates[0];
             }
             if(context.start_date)
-                context.start_date = context.start_datetime.toISOString().replace("T"," ").substring(0,16);
+                context.start_date = context.start_datetime.format("MM/DD/YYYY hh:mm a");
             if(context.end_date)
-                context.end_date = context.start_datetime.toISOString().replace("T"," ").substring(0,16);
+                context.end_date = context.end_datetime.format("MM/DD/YYYY hh:mm a");
 
             this.$el.html(temp_event_add(context));
             if(this.model.get("location_point")){
@@ -58,18 +60,17 @@ define([
                 marker = map.add_marker(null,true);
             }
             marker.on('dragend', function(e) {
-                $('#id_lat').val(e.target._latlng.lat);
-                $('#id_lng').val(e.target._latlng.lng);
+                $('#lat').val(e.target._latlng.lat);
+                $('#lng').val(e.target._latlng.lng);
             });
             marker.bindPopup("Move the maker to where the events will be").openPopup();
             //trigger resive event
             //set up datetime picker. destroy?
-            var startDateTextBox = this.$el.find("#id_start_date"),
-            endDateTextBox = this.$el.find("#id_end_date");
+            var startDateTextBox = this.$el.find("#start_date_input"),
+            endDateTextBox = this.$el.find("#end_date_input");
 
             startDateTextBox.datetimepicker({
                 timeFormat: "hh:mm tt",
-                minDate: new Date(),
                 onClose: function(dateText, inst) {
                     if (endDateTextBox.val() !== '') {
                         var testStartDate = startDateTextBox.datetimepicker('getDate');
@@ -105,28 +106,28 @@ define([
             });
             return this;
         },
-        onResize: function(){
-            if(this.originalHeight <  $(window).height()){
-                $(".form-actions").height($(window).height() - this.originalHeight);
+        onResize: function(e){
+            var self = e ? e.data : this;
+            if(self.originalHeight <  $(window).height()){
+                $(".form-actions").height($(window).height() - self.originalHeight);
             }else{
-                $(".form-actions").height(this.formActionHeight);
+                $(".form-actions").height(self.formActionHeight);
             }
         },
         onDOMadd: function(e){
             this.formActionHeight = $(".form-actions").height();
             this.originalHeight = $(".top").height() + $("#event_add_form").height() - this.formActionHeight;
-            this.onResize()
         },
         geocode: function() {
-            map.geocode($("#id_street").val() + " " + $("#id_city").val(), {
-                onSuccess: function(lat, lng) {
-                    $('#id_lat').val(lat);
-                    $('#id_lng').val(lng);
-                },
-                onFail: function() {
-                    alert("Could not Find, Please drag the Marker to the location of the event");
+            geocoder.mapquest($("#street_input").val() + ", " + $("#city_state_input").val(),
+                function(latlng, lng) {
+                    $('#lat').val(latlng.lat);
+                    $('#lng').val(latlng.lng);
+                    var newCenter = new L.LatLng(latlng.lat, latlng.lng);
+                    map.map.setView(newCenter, 15, false);
+                    map.marker.setLatLng(newCenter);
                 }
-            });
+            );
         },
         add_event: function(e){
             var self = this;
@@ -136,9 +137,9 @@ define([
             $(".label").hide();
             var json = Utils.form2object("#event_add_form");
             if(json.start_date)
-                json.start_date = json.start_date.replace(" ","T");
+                json.start_date = moment($("#start_date_input").datetimepicker('getDate'));
             if(json.end_date)
-                json.end_date = json.end_date.replace(" ","T");
+                json.end_date = moment($("#end_date_input").datetimepicker('getDate'));
             var promise = this.model.save(json,{isComplete:true});
             if(promise){
                 promise.error(function(response) {
