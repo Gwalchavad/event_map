4c537e3975b4b037dfba93524c0e0d53f8f51765
wanderer <mjbecze@gmail.com>
2013-02-21 20:06:17 -0500

    testings

--- event_map/static/src/js/views/list.js
+++ event_map/static/src/js/views/list.js
@@ -363,7 +363,7 @@ define([
                     this.$el.find("#month_"+month+"_"+year).children().text(text);
                     //test if the day exists
                     if(this.$el.find("#day_"+day+"_"+month+"_"+year).length === 0){
-                        console.log("inserting day", day);
+                        console.log("inserting day", day,this.$el.find("#day_"+nDay+"_"+nMonth+"_"+nYear),html_days);
                         this.$el.find("#day_"+nDay+"_"+nMonth+"_"+nYear)[insertMethod](html_days);
                     }else{
                         //expand day
